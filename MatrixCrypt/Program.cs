267889ae384c1d020b4cba9fb21f7e220c905361
Vladimir <56363775+jpegie@users.noreply.github.com>
2022-09-19 00:57:08 +0500

    changing arrays to matnet matrix

--- MatrixCrypt/Program.cs
+++ MatrixCrypt/Program.cs
@@ -1,8 +1,7 @@
-﻿int [,] kMatrix = 
-{ 
-    { 2, 1 }, 
-    { 9, 3 } 
-};
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+var kMatrix = DenseMatrix.OfArray(new [,] { { 2.0, 1.0 }, { 9.0, 3.0 } });
 
 string Enrypt(string str)
 {
@@ -14,20 +13,24 @@ string Enrypt(string str)
     var couples = MakeCouples(str);
     foreach(var couple in couples)
     {
-        var cryptedCouple = MultiplyCoupleToKMatrix(couple);
+        var cryptedCouple = MultiplyCoupleToMatrix(couple, kMatrix);
         encryptedStr += cryptedCouple;
     }
     return encryptedStr;
 }
 
-string MultiplyCoupleToKMatrix(string couple)
+string MultiplyCoupleToMatrix(string couple, Matrix<double> matrix)
 {
     var coupleArr = couple.ToCharArray();
+    var coupleMatrix = DenseMatrix.OfArray(new[,] 
+    { 
+        { (double)coupleArr[0] }, 
+        { (double)coupleArr[1] } 
+    });
+    var resultMatrix = (DenseMatrix)matrix.Multiply(coupleMatrix);
     var result = new char[2];
-    for(int i = 0; i < 2; ++i)
-    {
-        result[i] = (char)((kMatrix[i, 0] * coupleArr[0] + kMatrix[i, 1] * coupleArr[1]) % 256);
-    }
+    result[0] = (char)(resultMatrix.Column(0)[0] % 256);
+    result[1] = (char)(resultMatrix.Column(0)[1] % 256);   
     return new string(result);
 }
 
@@ -41,78 +44,47 @@ List<string> MakeCouples(string str)
     return couples;
 }
 
-int [,] GetTransposedKMatrix()
-{
-    int[,] kMatrixTransposed = new[,] 
-    { 
-        { 0, 0 }, 
-        { 0, 0 } 
-    };
-    for (int i = 0; i < 2; ++i)
-    {
-        for (int j = 0; j < 2; ++j)
-        {
-            kMatrixTransposed[i, j] = kMatrix[j, i];
-        }
-    }
-    return kMatrixTransposed;
-}
-
 string Decrypt(string str)
 {
-   
     var decryptingMatrix = GetMatrixForDecrypt();
     var couples = MakeCouples(str);
     var decryptedStr = "";
     foreach(var couple in couples)
     {
-        decryptedStr += DecryptCouple(couple, decryptingMatrix);
+        decryptedStr += MultiplyCoupleToMatrix(couple, decryptingMatrix);
     }
     return decryptedStr;
 }
 
-int [,] GetMatrixForDecrypt()
-{
-    int[,] result = new[,]
-    {
-        { 0, 0 },
-        { 0, 0 }
-    };
-    var kMatrixTransposed = GetTransposedKMatrix();
-    var inversedMatrix = (int[,])kMatrixTransposed.Clone();
-    var kMatrixDet = kMatrix[0,0]*kMatrix[1,1] - kMatrix[0,1]*kMatrix[1,0];
+Matrix<double> GetMatrixForDecrypt()
+{ 
+    var result = new DenseMatrix(2, 2);
+    var kMatrixDet = kMatrix.Determinant();
+    var kMatrixInversed = kMatrix.Inverse();
+    kMatrixInversed.Multiply(kMatrixDet, kMatrixInversed);
 
-    inversedMatrix[0, 0] = kMatrixTransposed[1, 1];
-    inversedMatrix[0, 1] = -kMatrixTransposed[1, 0];
-    inversedMatrix[1, 0] = -kMatrixTransposed[0, 1];
-    inversedMatrix[1, 1] = kMatrixTransposed[0, 0];
-    
     int x, y;
-    Gcd(kMatrixDet, 256, out x, out y);
+    GCD((int)kMatrixDet, 256, out x, out y);
 
-    for (int i=0;i<2 ; ++i)
+    for (int i=0;i<2; ++i)
     {
         for(int j=0;j<2 ; ++j)
         {
-            inversedMatrix[i,j] *= x;
-            inversedMatrix[i, j] = (int)((uint)inversedMatrix[i, j] % 256);
+            kMatrixInversed[i,j] *= x;
+            if (kMatrixInversed[i,j] < 0)
+            {
+                kMatrixInversed[i, j] = (uint)Math.Floor(kMatrixInversed[i, j]) % 256;
+            }
+            else
+            {
+                kMatrixInversed[i, j] = (uint)Math.Ceiling(kMatrixInversed[i, j]) % 256;
+            }
         }
     }
-    return inversedMatrix;
+    return kMatrixInversed;
 }
 
-
-string DecryptCouple(string couple, int [,] decryptMatrix)
-{
-    char[] decryptedChars = new char[2];
-    for(int i=0;i<2 ; ++i)
-    {
-        decryptedChars[i] = (char)((decryptMatrix[i,0] * couple[0] + decryptMatrix[i, 1] * couple[1])%256);
-    }
-    return new string(decryptedChars);
-}
-
-int Gcd(int a, int b, out int x, out int y)
+int GCD(int a, int b, out int x, out int y)
 {
 
     if (a == 0)
@@ -122,7 +94,7 @@ int Gcd(int a, int b, out int x, out int y)
         return b;
     }
 
-    int gcd = Gcd(b % a, a, out x, out y);
+    int gcd = GCD(b % a, a, out x, out y);
 
     int newY = x;
     int newX = y - (b / a) * x;
