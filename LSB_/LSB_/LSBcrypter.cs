e1350f3c8093c27ad43af9be9c91c0d289c0eb8c
Vladimir <56363775+jpegie@users.noreply.github.com>
2022-11-24 21:14:15 +0500

    добавляю нормальный проект с LSB

---
+++ LSB_/LSB_/LSBcrypter.cs
@@ -0,0 +1,117 @@
+﻿using System.Drawing;
+
+namespace LSB_;
+public static class LSBcrypter
+{
+    public static Bitmap HideData(Bitmap image, IEnumerable<byte> data)
+    {
+        var bytes = data.ToList();
+        var currByte = 0;
+        var bitsWritten = 0; //8 -> Байт записали
+        var isWrittingOnlyRG = false;
+
+        for (int x = 0; x < image.Width; ++x)
+        {
+            for (int y = 0; y < image.Height; ++y)
+            {
+                Color imagePixel = image.GetPixel(x, y);
+
+                var r = imagePixel.R - imagePixel.R % 2;
+                var g = imagePixel.G - imagePixel.G % 2;
+                var b = imagePixel.B - imagePixel.B % 2;
+
+                if (currByte == bytes.Count)
+                {
+                    return image;
+                }
+                else
+                {
+                    if (bitsWritten == 6)
+                    {
+                        isWrittingOnlyRG = true;
+                    }
+
+                    r += CutByte(bytes, currByte, ref bitsWritten);
+                    g += CutByte(bytes, currByte, ref bitsWritten);
+
+                    if (!isWrittingOnlyRG)
+                    {
+                        b += CutByte(bytes, currByte, ref bitsWritten);
+                    }
+                }
+
+                image.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                if (bitsWritten == 8)
+                {
+                    bitsWritten = 0;
+                    isWrittingOnlyRG = false;
+                    currByte++;
+                }
+            }
+        }
+        return image;
+    }
+
+    private static int CutByte(List<byte> bytes, int i, ref int bitsWritten)
+    {
+        var cutten = bytes[i] % 2;
+        bytes[i] /= 2;
+        bitsWritten++;
+        return cutten;
+
+    }
+    public static IEnumerable<byte> RevealData(Bitmap image, int bytesHidden)
+    {
+        List<bool> bits = new List<bool>();
+        List<byte> hiddenBytes = new List<byte>();
+
+        for(int x = 0; x < image.Width ; ++x)
+        {
+            for(int y = 0; y < image.Height; ++y)
+            {
+                if (hiddenBytes.Count == bytesHidden)
+                {
+                    return hiddenBytes;
+                }
+
+                Color imagePixel = image.GetPixel(x, y);
+
+                bits.Add(imagePixel.R % 2 == 1);
+                bits.Add(imagePixel.G % 2 == 1);
+
+                if (bits.Count == 8)
+                {
+                    bits.Reverse();
+                    hiddenBytes.Add(ConvertBoolArrayToByte(bits.ToArray()));
+                    bits.Clear();    
+                }
+                else
+                {
+                    bits.Add(imagePixel.B % 2 == 1);
+                }
+            }
+        }
+
+        return hiddenBytes;
+    }
+
+    private static byte ConvertBoolArrayToByte(bool[] source)
+    {
+        byte result = 0;
+        // This assumes the array never contains more than 8 elements!
+        int index = 8 - source.Length;
+
+        // Loop through the array
+        foreach (bool b in source)
+        {
+            // if the element is 'true' set the bit at that position
+            if (b)
+                result |= (byte)(1 << (7 - index));
+
+            index++;
+        }
+
+        return result;
+    }
+}
---
+++ LSB_/LSB_/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using LSB_;
+
+var input = "0";
+
+do
+{
+    Console.Write("Enter 0 for hide, 1 for reveal, 2 for exit: ");
+    input = Console.ReadLine();
+
+    if (input == "0")
+    {
+        Console.Write("Enter text to hide: ");
+        var textBytes = Encoding.UTF8.GetBytes(Console.ReadLine()!);
+
+        Console.Write("Image path (where to hide text): ");
+        var imagePath = Console.ReadLine();
+
+        var image = new Bitmap(Image.FromFile(imagePath));
+
+
+        if (!IsAbleToHide(textBytes, image))
+        {
+            Console.WriteLine("Text's size > than possible to hide in the image");
+            continue;
+        }
+
+        var modifiedImage = LSBcrypter.HideData(image, textBytes);
+        var newPath = imagePath.Insert(imagePath.LastIndexOf("."), "_modified");
+        modifiedImage.Save(newPath);
+
+        Console.WriteLine($"Image saved into {newPath}" +
+                          $"\nAmount of hidden bytes: {textBytes.Length}");
+    }
+    else if (input == "1")
+    {
+        Console.Write("Image path (with hidden text): ");
+        var image = new Bitmap(Image.FromFile(Console.ReadLine()!));
+
+        Console.Write("Amount of hidden bytes: ");
+        var hiddenData = LSBcrypter.RevealData(image, int.Parse(Console.ReadLine()!));
+
+        var hiddenText = Encoding.UTF8.GetString(hiddenData.ToArray());
+        Console.WriteLine($"Hidden text in image: {hiddenText}");
+    }
+    Console.WriteLine("\n#\n");
+}
+while (input != "3");
+
+bool IsAbleToHide(IEnumerable<byte> data, Bitmap image)
+{
+    //каждый пиксель = 3 бита информации
+    //все пиксели * 3 / 8 = кол-во байт, которые можем туда запихать
+    if (image.Width * image.Height * 3 / 8 < data.Count())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
