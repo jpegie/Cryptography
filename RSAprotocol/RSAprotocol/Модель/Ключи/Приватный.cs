dcbf57528d483bba0d0d7e369c63ca5e53a3998c
Vladimir <56363775+jpegie@users.noreply.github.com>
2023-05-22 21:48:48 +0500

    init
    Добавляю проект с описанием протокола на основе шифрования RSA и с подписью RSA

---
+++ RSAprotocol/RSAprotocol/Модель/Ключи/Приватный.cs
@@ -0,0 +1,7 @@
+﻿using System.Numerics;
+
+public class PrivateKey
+{
+    public BigInteger D { get; set; }
+    public BigInteger N { get; set; }
+}
---
+++ RSAprotocol/RSAprotocol/Модель/Сообщение.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+public class Message
+{
+    public BigInteger ID { get; set; }
+    public string Sender { get; set; }
+    public Byte[] Data { get; set; }
+    public BigInteger[] SignedData { get; set; }
+}
+
+public class EncryptedSignedMessage
+{
+    public BigInteger[] Data { get; set; }
+}
